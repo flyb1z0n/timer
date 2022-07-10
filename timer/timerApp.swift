3959970bb801cbff5cb5e1c1c9e765d1c95d0009
Kostiantyn Shavrukov <kostiantyn.shavrukov@grammarly.com>
2022-07-10 22:05:19 +0200

    Initial Commit

---
+++ timer/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer
+//
+//  Created by Kostiantyn Shavrukov on 10.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
